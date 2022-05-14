57a6685c22bfcc5a4c239cea2415a306f5d4d092
Jacob Piland <piland.jacob@gmail.com>
2022-05-14 18:40:26 -0500

    Add destroy spec

---
+++ spec/features/items/destroy_spec.rb
@@ -0,0 +1,20 @@
+require 'rails_helper'
+require 'faker'
+
+RSpec.describe 'Item Index' do
+  before :each do
+    @item = FactoryBot.create(:item)
+  end
+
+  describe 'the item destroy' do
+    it 'destroys the item' do
+      visit '/items'
+
+      click_on "Delete #{@item.name}"
+
+      expect(page).to_not have_content(@item.name)
+      expect(page).to_not have_content(@item.description)
+      expect(page).to_not have_content("$#{@item.format_price}")
+    end
+  end
+end
