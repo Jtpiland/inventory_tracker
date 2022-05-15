d4ed6306bc2edb7a4a73fcc9b8c7c44bd0dfd659
Jacob Piland <piland.jacob@gmail.com>
2022-05-15 13:18:58 -0500

    Add relationship and validation spec

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,6 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe Item, type: :model do
+  describe 'relationships' do
+    it { should have_one(:inventory_item) }
+  end
+
   describe 'validations' do
     it { should validate_presence_of(:name) }
     it { should validate_presence_of(:description) }
--- spec/models/warehouse_spec.rb
+++ spec/models/warehouse_spec.rb
@@ -1,6 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe Warehouse, type: :model do
+  describe 'relationships' do
+    it { should have_many(:inventory_items) }
+  end
+
   describe 'validations' do
     it { should validate_presence_of(:name) }
     it { should validate_presence_of(:city) }
