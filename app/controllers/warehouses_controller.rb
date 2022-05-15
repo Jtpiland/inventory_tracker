b2c127bc294fb7754241f8da3168178231f4554d
Jacob Piland <piland.jacob@gmail.com>
2022-05-15 12:15:10 -0500

    Add destroy functionality to pass spec

--- app/controllers/warehouses_controller.rb
+++ app/controllers/warehouses_controller.rb
@@ -13,6 +13,11 @@ class WarehousesController < ApplicationController
     end
   end
 
+  def destroy
+    Warehouse.find(params[:id]).destroy
+    redirect_to '/warehouses'
+  end
+
   private
 
   def warehouse_params
