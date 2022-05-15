046be89c76a6e012fd7be5b999c9372bc84cb658
Jacob Piland <piland.jacob@gmail.com>
2022-05-15 12:08:33 -0500

    Add warehouse destroy spec

---
+++ spec/features/warehouses/destroy_spec.rb
@@ -0,0 +1,22 @@
+require 'rails_helper'
+
+RSpec.describe 'Warehouse Index' do
+  before :each do
+    @warehouse = FactoryBot.create(:warehouse)
+  end
+
+  describe 'the warehouse destroy' do
+    it 'destroys the warehouse' do
+      visit '/warehouses'
+      expect(page).to have_content(@warehouse.name)
+      expect(page).to have_content(@warehouse.city)
+      expect(page).to have_content(@warehouse.state)
+
+      click_on "Delete #{@warehouse.name}"
+
+      expect(page).to_not have_content(@warehouse.name)
+      expect(page).to_not have_content(@warehouse.city)
+      expect(page).to_not have_content(@warehouse.state)
+    end
+  end
+end
